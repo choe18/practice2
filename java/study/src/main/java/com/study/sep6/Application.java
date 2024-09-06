d894f8cd7b2fbfc32616f4e25ff16c11216499a8
choe <rlorlrol562@gmail.com>
2024-09-06 12:55:18 +0900

    0906 code

---
+++ java/study/src/main/java/com/study/sep6/Application.java
@@ -0,0 +1,10 @@
+package com.study.sep6;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        method m = new method();
+        m.method();
+    }
+}
---
+++ java/study/src/main/java/com/study/sep6/method.java
@@ -0,0 +1,21 @@
+package com.study.sep6;
+
+public class method {
+
+    public void method() {
+
+        //  7~15 사이의 난수를 발생시켜서 3부터 발생한 난수까지의 합계 구하기
+
+        int random = (int) (Math.random() * 9) + 7;
+
+        System.out.println("random = " + random);
+
+        int sum = 0;
+
+        for (int i = 3; i < random; i++) {
+            sum += i;
+        }
+
+        System.out.println("3부터 발생한 " + random + "까지의 합 = " + sum);
+    }
+}
